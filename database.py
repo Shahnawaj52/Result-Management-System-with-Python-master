882e3412d375b2959f351e7d2c078ce8e1690a53
Shahnawaj52 <limon15-5736@diu.edu.bd>
2024-05-18 13:55:12 +0600

    new

---
+++ database.py
@@ -0,0 +1,31 @@
+
+import sqlite3
+def create_database():
+
+        connect = sqlite3.connect(database="rms.db")
+        cursor = connect.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS course (
+                            cid INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT,
+                            duration INTEGER,
+                            charges INTEGER,
+                            description TEXT
+                        )''')
+        connect.commit()
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS course (
+                            cid INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT,
+                            email TEXT,
+                            gender TEXT,
+                            birthday TEXT,
+                            contact INTEGER,
+                            course TEXT,
+                            address TEXT,
+                       
+                        )''')
+        connect.commit()
+        print("Database and table created successfully.")
+
+if __name__ == "__main__":
+    create_database()
